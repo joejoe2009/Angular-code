717d3f0b1cde44820ce828a87e05198821b30f95
joejoe2009 <rexgmann@gmail.com>
2022-08-24 19:23:09 -0700

    Angular code ( First Commit)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,58 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles:[`
+  // h3{
+  //   color: dodgerblue;
+  // }
+  
+  // `]
 })
 export class AppComponent {
-  title = 'my-first1-app';
+  // username = '';
+  // showSecret = false;
+  // log = [];
+  // serverElements = [{type: 'server', name: 'TestServer', content: 'Just a test'}]; 
+
+  // onServerAdded(serverData: {serverName: string, serverContent: string}) {
+  //   this.serverElements.push({
+  //     type: 'server',
+  //     name: serverData.serverName,
+  //     content: serverData.serverContent
+  //   });
+  // }
+  // onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+  //   this.serverElements.push({
+  //     type:'blueprint',
+  //     name: blueprintData.serverName,
+  //     content:blueprintData.serverContent
+  //   })
+
+  // }
+
+  // onChangeFirst() {
+  //   this.serverElements[0].name = 'Changed!';
+  // }
+
+  // onDestroyFirst() {
+  //   this.serverElements.splice(0, 1);
+  // }
+
+  // onToggleDetails() {
+  //   this.showSecret = !this.showSecret;
+  //   //this.log.push(this.log.length + 1); 
+  //   this.log.push(new Date()); 
+  // } 
+
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+    CockpitComponent,
+    ServerElementComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
